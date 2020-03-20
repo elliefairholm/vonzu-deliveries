bb639bd2cbdaa52564647fbb269c583be920771f
elliefairholm <elliefairholm@gmail.com>
2020-03-20 12:17:57 +0100

    feat: header bar added

---
+++ client/src/components/presentational/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+import logo from '../../../assets/logo.png'
+
+function Header () {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={logo} alt="Vonzu Logo" />
+      </div>
+    </div>
+  );
+
+}
+
+export default Header
+  ;
